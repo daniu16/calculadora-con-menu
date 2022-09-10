90e7bb5e1b734c09d08a0e97da4d279140bf49a4
daniu16 <cesar2224642@correo.uis.edu.co>
2022-09-10 08:58:08 -0500

    se ha agregado el enunciado

--- calculadora-menu.py
+++ calculadora-menu.py
@@ -1,6 +1,8 @@
 # calculadora con menú
 
-from math import log 
+from math import log
+from operator import truediv
+from tkinter import Y 
 
 print("--------------------------------------")
 print("----------calculadora-menu------------")
@@ -20,4 +22,38 @@ print("4. dividir (el primero sobre el segundo)")
 print("5. potencia(el primero a la potencia del segundo)")
 print("6. logaritmo (el logaritmo del primero)")
 
-opcion=int(input("\nDame la opcion"))
+opcion=int(input("\nDame la opcion: "))
+
+#processing
+if(opcion) == 1:
+    z=x + y
+    print(x,"+",y)
+elif (opcion == 2):
+    z = x - y
+    print(x ,"-", y)
+elif (opcion == 3):
+    z= x * y
+    print(x,"*",y)
+elif (opcion == 4):
+    z= x/y 
+    print(x,"/",y)
+elif (opcion == 4 and y ==0):
+    print("el denomindor es igual a cero y")
+    print("NO se puede realizar la divicion")
+elif(opcion == 5):
+    z = pow (x,y)
+    print(x,"^",y)
+elif(opcion == 6 and x>0):
+    z = log(x)
+    print("logaritmo de", x)
+elif (opcion == 6 and x <=0):
+    print("el valorde c es <= a cero y")
+    print("NO se puede calcular el logaritmo")
+    bandera= True
+else:
+    print("opcion no valida")
+#se escribe el resultado con otra condicion
+if(opcion <7 and bandera == False):
+    print("resultado=", z)
+
+# fin 
